bdd8eb5a3a2df6b5f3f0dcff4ac136b72ef6f60f
randhirgws <randhir.gws@gmail.com>
2021-02-11 16:42:02 +0530

    TheDiceGameJS

--- TheDiceGameJS/assets/js/shuffleDice.js
+++ TheDiceGameJS/assets/js/shuffleDice.js
@@ -1,16 +1,15 @@
 function diceShuffle(){
    var count_dice1=(Math.floor((Math.random() * 6) + 1));
    var count_dice2=(Math.floor((Math.random() * 6) + 1));
-
-   console.log("<img  src="+"..\\diceImages\\dice"+count_dice2+".jpg");
+   console.log("<img  src="+"..\\assets\\diceImages\\dice"+count_dice2+".jpg");
    //adding image1
     var img=document.createElement("img");
-    img.src = "..\\diceImages\\dice"+count_dice1+".jpg"; 
+    img.src = "..\\assets\\diceImages\\dice"+count_dice2+".jpg"
     document.getElementById("disp1").append(img);
 
     //adding image2
     var img2=document.createElement("img");
-    img2.src = "..\\diceImages\\dice"+count_dice2+".jpg"; 
+    img2.src ="..\\assets\\diceImages\\dice"+count_dice2+".jpg"
     document.getElementById("disp2").append(img2);
 
     if(count_dice1>count_dice2){
